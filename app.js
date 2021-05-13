4e244ea73586042f40471552505571753dc2ceb8
weirdyang <wy4786@gmail.com>
2021-05-13 21:42:56 +0800

    removing domain

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ const corsOptions = {
   origin: process.env.CLIENT_URL,
   credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  allowedHeaders: ['Origin', 'Content-type', 'Accept', 'Authorization', 'x-xsrf-token'],
 };
 debug(process.env.CLIENT_URL);
 app.use(cors(corsOptions));
