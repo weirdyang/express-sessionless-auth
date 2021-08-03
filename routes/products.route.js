8f156079515596bbad18bb1e9933b3e37992144e
weirdyang <wy4786@gmail.com>
2021-08-03 16:18:18 +0800

    feat(products): renaming routes to standardize

--- routes/products.route.js
+++ routes/products.route.js
@@ -29,10 +29,12 @@ const fileCheck = check('file')
   .withMessage('Please ensure a file is attached, and is png or jpg format');
 const nameCheck = check('name').not().isEmpty().bail();
 const descriptionCheck = check('description').not().isEmpty().bail();
+const productTypeCheck = check('productType').not().isEmpty().bail();
 const reqChecks = [
   nameCheck,
   descriptionCheck,
   fileCheck,
+  productTypeCheck,
 ];
 // all requests must be authenticated
 router.use(auth.jwt);
@@ -43,15 +45,15 @@ router.post('/create', uploadStrategy, reqChecks, createProduct);
 // return all products
 router.get('/', fetchAllProducts);
 // get specific journal
-router.get('/:id', fetchProductById);
+router.get('/details/:id', fetchProductById);
 // get products for user
 router.get('/user/self', fetchProductsForProfile);
-// todo: future feature
+// get products for user by userId
 router.get('/user/:id', fetchProductsByUser);
 // update product entry
 router.put('/:id', uploadStrategy, reqChecks, updateProduct);
 // update product details
-router.put('/details/:id', [nameCheck, descriptionCheck], updateProduct);
+router.put('/details/:id', [nameCheck, descriptionCheck, productTypeCheck], updateProduct);
 // update product image only
 router.put('/image/:id', uploadStrategy, [fileCheck], updateProduct);
 // delete
